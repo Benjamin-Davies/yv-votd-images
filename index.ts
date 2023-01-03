3ebc12b036a197ce766ee0d9341277d0bd77517b
Benjamin Davies <bentendavies@gmail.com>
2023-01-03 18:37:15 +1300

    Load page content

---
+++ index.ts
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+import { readFile } from 'fs/promises';
+
+const PAGE_URL = 'https://www.bible.com/verse-of-the-day';
+
+async function fetchPage(): Promise<string> {
+  // TODO: remove when finished testing
+  return await readFile('example-response.html', 'utf-8');
+
+  const res = await fetch(PAGE_URL);
+  const text = await res.text();
+  return text;
+}
+
+fetchPage().then(console.log);
